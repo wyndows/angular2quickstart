fed400c1f742c9eb820299138af25ecd1498eb29
Marlan Ball <wyndows2k@earthlink.net>
2016-04-24 14:01:34 -0600

    fixed root path

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,7 +2,7 @@ import {Component} from "angular2/core";
 
 @Component({
 	selector: "my-app",
-	templateUrl: "templates/splash-page.html"
+	templateUrl: "app/templates/splash-page.html"
 })
 
 export class AppComponent {}
